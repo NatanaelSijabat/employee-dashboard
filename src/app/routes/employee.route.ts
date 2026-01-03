24398870a248a5edd82ade6f9c6adba21edf246a
nael <naellabit@gmail.com>
2026-01-03 14:20:31 +0700

    feat : edit employee

--- src/app/routes/employee.route.ts
+++ src/app/routes/employee.route.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { EmployeeListComponent } from '../components/employee-list/employee-list.component';
+import { EmployeeListComponent } from '../employee-list/employee-list.component';
 
 export const employeeRoutes: Routes = [
   { path: 'employees', component: EmployeeListComponent },
